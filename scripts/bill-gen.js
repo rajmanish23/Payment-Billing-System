f4340fa59605d0fc1d7f06ee3faaed668de64a0d
Raj Manish <katrajmanish23@gmail.com>
2022-05-23 19:48:10 +0530

    "tried" doing css and had an emotinal attack...

--- scripts/bill-gen.js
+++ scripts/bill-gen.js
@@ -4,7 +4,8 @@ document.getElementById("addBillItem").addEventListener("click", function () {
          <div class="innerBillElement">
             <label for="itemId">Item Code </label>
             <input type="number" id="itemId" name="itemId">
-            <div id="itemName"></div> <!-- JS function fetches item name from item code and puts it here -->
+            <label for="itemName">Item </label>
+            <input type="text" id="itemName" name="itemName">
             <label for="itemPrice"> Price </label>
             <input type="number" id="itemPrice" name="itemPrice">
             <label for="itemQty">Quantity </label>
