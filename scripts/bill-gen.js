c30b09354b7a63315cda40e5010ed51fd3f89ab1
Raj Manish <katrajmanish23@gmail.com>
2022-07-15 06:32:04 +0530

    trying to prevent field clearing in bill-gen

--- scripts/bill-gen.js
+++ scripts/bill-gen.js
@@ -1,17 +1,19 @@
 // adds an empty input for next bill item
-document.getElementById("addBillItem").addEventListener("click", function () {
+
+function addBillItem() {
     document.getElementById("innerBillDetails").innerHTML += `
-         <div id="innerBillElementNew">
-            <label for="itemId">Item Code </label>
-            <input type="number" id="itemId" name="itemId">
-            <label for="itemName">Item </label>
-            <input type="text" id="itemName" name="itemName">
-            <label for="itemPrice"> Price </label>
-            <input type="number" id="itemPrice" name="itemPrice">
-            <label for="itemQty">Quantity </label>
-            <input type="number" id="itemQty" name="itemQty">
+        <div id="innerBillElementNew">
+        <label for="itemId">Item Code </label>
+        <input type="number" id="itemId" name="itemId">
+        <label for="itemName">Item </label>
+        <input type="text" id="itemName" name="itemName">
+        <label for="itemPrice"> Price </label>
+        <input type="number" id="itemPrice" name="itemPrice">
+        <label for="itemQty">Quantity </label>
+        <input type="number" id="itemQty" name="itemQty">
         </div>
         <br>
-    `
-});
-
+        `;
+    // return false;
+}
+    
